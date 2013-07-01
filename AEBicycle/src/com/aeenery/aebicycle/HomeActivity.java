c4c704ea62fab19d4d4faf08e85a256b2db45e38
jianxing <kobeonetow@163.com>
2013-07-01 17:25:03 +0800

    try again

--- AEBicycle/src/com/aeenery/aebicycle/HomeActivity.java
+++ AEBicycle/src/com/aeenery/aebicycle/HomeActivity.java
@@ -79,8 +79,7 @@ public class HomeActivity extends Activity{
 	private void startPushService() {
 		if(D) Log.i(TAG,"++ In PUSH SERVICE ++");
 		if(!PushManager.isPushEnabled(getApplicationContext())){
-			PushSettings.enableDebugMode(getApplicationContext(), true);
-			PushManager.tryConnect(getApplicationContext());
+//			PushSettings.enableDebugMode(getApplicationContext(), true);
 			PushManager.startWork(getApplicationContext(), PushConstants.LOGIN_TYPE_API_KEY, BicycleUtil.PUSH_API_KEY);
 		}
 	}

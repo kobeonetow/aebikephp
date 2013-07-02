2a06b0fccf70afc8312d1db276c48a5827ed20a1
jianxing <kobeonetow@163.com>
2013-07-02 17:38:41 +0800

    add the checkbox selection for friend list

--- AEBicycle/src/com/aeenery/aebicycle/friend/FriendListActivity.java
+++ AEBicycle/src/com/aeenery/aebicycle/friend/FriendListActivity.java
@@ -17,9 +17,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,65 +33,92 @@ public class FriendListActivity extends BaseActivity {
 	private ServerAPI api = null;
 	private ListView lv = null;
 	private Useraccount[] friends = null;
-	
+	private Button btnSend = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		api = new ServerAPI(this, LoginActivity.user);
 		setContentView(R.layout.activity_friend_list);
-		
+
 		initFriendListView();
 	}
 
-	//Get the friend list for user
+	// Get the friend list for user
 	private void initFriendListView() {
-		api.getFriendList(this,"0");
-		lv = (ListView)findViewById(R.id.view_friend_listview);
+		api.getFriendList(this, "0");
+		lv = (ListView) findViewById(R.id.view_friend_listview);
 		lv.setOnItemClickListener(new FriendClickListener());
+
+		btnSend = (Button) findViewById(R.id.btnSendInvite);
+		btnSend.setOnClickListener(sendButtonClick());
 	}
 
 	/**
 	 * Set friends to the view
+	 * 
 	 * @param json
 	 */
-	public void setFriendsToView(JSONObject json){
-		try{
-		JSONArray json2 = json.getJSONArray("data");
-		friends = new Useraccount[json2.length()];
-		for(int i=0; i<json2.length(); i++){
-			JSONObject curIndex = ((JSONObject)json2.get(i));
-			Useraccount user = new Useraccount();
-			user.__setUserFromJSONObject(curIndex);
-			friends[i] = user;
-			
-		}
-		lv.setAdapter(new FriendViewAdapter(FriendListActivity.this, R.layout.listview_friend_adapter, friends));
-		}catch(Exception e){
-			Log.i("FriendListActivity",e.getMessage());
+	public void setFriendsToView(JSONObject json) {
+		try {
+			JSONArray json2 = json.getJSONArray("data");
+			friends = new Useraccount[json2.length()];
+			for (int i = 0; i < json2.length(); i++) {
+				JSONObject curIndex = ((JSONObject) json2.get(i));
+				Useraccount user = new Useraccount();
+				user.__setUserFromJSONObject(curIndex);
+				friends[i] = user;
+
+			}
+			lv.setAdapter(new FriendViewAdapter(FriendListActivity.this,
+					R.layout.listview_friend_adapter, friends));
+		} catch (Exception e) {
+			Log.i("FriendListActivity", e.getMessage());
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.friend_list, menu);
 		return true;
 	}
-	
-	class FriendClickListener implements OnItemClickListener{
+
+	class FriendClickListener implements OnItemClickListener {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
-			//Do nothing at the moment
+			// Do nothing at the moment
 		}
 	}
-	
-	class FriendViewAdapter extends ArrayAdapter<Useraccount>{
 
-		protected Context  context;
-		protected int resourceId ;
+	protected OnClickListener sendButtonClick() {
+		return new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				 String inviteList = "";
+				for(int i=0;i<friends.length;i++){
+					 View child = lv.getChildAt(i);
+					 Useraccount ac = friends[i];
+					 CheckBox cb = (CheckBox)child.findViewById(R.id.is_invite_friend);
+					 if(cb.isChecked()){
+						 inviteList += ac.getId()+",";
+					 }
+				}
+				if(inviteList.length()>0){
+					inviteList = inviteList.substring(0, inviteList.length()-1);
+					Log.i("String is",inviteList);
+				}
+			}
+		};
+	}
+
+	class FriendViewAdapter extends ArrayAdapter<Useraccount> {
+
+		protected Context context;
+		protected int resourceId;
 		protected Useraccount[] friends;
-		
+
 		public FriendViewAdapter(Context context, int textViewResourceId,
 				Useraccount[] friends) {
 			super(context, textViewResourceId, friends);
@@ -97,21 +126,23 @@ public class FriendListActivity extends BaseActivity {
 			this.resourceId = textViewResourceId;
 			this.friends = friends;
 		}
-		
+
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent){
+		public View getView(int position, View convertView, ViewGroup parent) {
 			View row = convertView;
-			if(row == null){
-				LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-				row = inflater.inflate(resourceId, parent,false);
+			if (row == null) {
+				LayoutInflater inflater = ((Activity) context)
+						.getLayoutInflater();
+				row = inflater.inflate(resourceId, parent, false);
 			}
 			Useraccount f = friends[position];
-			TextView name = (TextView)row.findViewById(R.id.friend_name);
-			CheckBox checkBox = (CheckBox)row.findViewById(R.id.is_invite_friend);
+			TextView name = (TextView) row.findViewById(R.id.friend_name);
+			CheckBox checkBox = (CheckBox) row
+					.findViewById(R.id.is_invite_friend);
 			name.setText(f.getName());
 			return row;
 		}
-		
+
 	}
 
 }
